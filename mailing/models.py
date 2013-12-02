ca41855dd79ca07441a5623509de368d71444ed9
ovidner <ovid@Merlin.local>
2013-12-02 23:48:23 +0100

    Småfix

--- mailing/models.py
+++ mailing/models.py
@@ -9,10 +9,10 @@ class MailingList(models.Model):
     description = models.TextField(max_length=256, blank=True)
     closed = models.BooleanField(default=False)
     admins = models.ManyToManyField(User, null=True, blank=True, related_name="mailinglist_admin_set")
-    members = models.ManyToManyField(User, through='Membership', null=True, blank=True, related_name="mailinglist__set") # Varför inte grupp? För att alla användare var och en ska kunna välja bort listor man inte vill vara med i även om skriptet tycker att de borde vara med.
+    members = models.ManyToManyField(User, through='Membership', null=True, blank=True, related_name="mailinglist_member_set") # Varför inte grupp? För att alla användare var och en ska kunna välja bort listor man inte vill vara med i även om skriptet tycker att de borde vara med.
     
     def __unicode__(self):
-        return '{0} - {1}'.format(self.address, self.name)
+        return '{0} – {1}'.format(self.address, self.name)
     
 
 class Membership(models.Model):
