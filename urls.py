9cf775a4fac756ba0211dfbf403d79eb9df7713d
Gustav Häger <hager.gustav@gmail.com>
2013-12-02 11:15:13 +0100

    Fånig commit, för att testa...

--- urls.py
+++ urls.py
@@ -5,6 +5,9 @@ from django.contrib import admin
 from mezzanine.core.views import direct_to_template
 
 
+#Detta ser lite autogenererat ut...
+#skriver man verkligen kommentarer på det här sättet btw...
+
 admin.autodiscover()
 
 # Add the urlpatterns for any custom Django applications here.
