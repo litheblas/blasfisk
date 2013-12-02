05ccea831fd2d8ff416dc686c349c4e33efd2e1b
ovidner <ovid@Merlin.local>
2013-12-02 23:48:52 +0100

    Grundutförande

--- barenbaren/models.py
+++ barenbaren/models.py
@@ -1,6 +1,25 @@
 from django.db import models
-from litheblas.events.models import Event as BasenEvent
+from litheblas.blasbasen.models import User
+from litheblas.events.models import Attendance
 
-class Event(models.Model):
-    basen_event = models.OneToOneField(BasenEvent)
+#class Event(models.Model):
+#    pass
+
+class Balance(models.Model):
+    user = models.OneToOneField(User)
+    balance = models.DecimalField(max_digits=9, decimal_places=2)
     
+    def __unicode__(self):
+        return u'{0}: {1}'.format(self.user.get_short_name(), self.balance)
+
+class Consumption(models.Model):
+    """Knyter samman användare, evenemang, dag och streck"""
+    event_user = models.ForeignKey(Attendance)
+    date = models.DateField()
+    amount = models.PositiveIntegerField()
+    
+    class Meta:
+        unique_together = [['event_user', 'date'],]
+    
+    def __unicode__(self):
+        return u'{0}: {1}, {2} cl'.format(self.date, self.event_user, self.amount)
