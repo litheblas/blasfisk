6d5c2428adc05a0c9f5121614eb5ee70060f2ab0
ovidner <ovid@c-204-135.eduroam.liu.se>
2013-12-02 11:22:49 +0100

    Olle trollar

--- urls.py
+++ urls.py
@@ -4,9 +4,10 @@ from django.contrib import admin
 
 from mezzanine.core.views import direct_to_template
 
-
+#Bajsbajsbajs /Ollé
 #Detta ser lite autogenererat ut...
-#skriver man verkligen kommentarer på det här sättet btw...
+#skriver man verkligen kommentarer på det här sättet btw... Wöö Olle trollar
+#Hejhej
 
 admin.autodiscover()
 
