1b096b20954919c8071b9ed2fe97d51296349fea
ovidner <olle@vidner.se>
2014-01-25 01:48:39 +0100

    Förbättrade adminmodeller

--- blasbasen/admin.py
+++ blasbasen/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 from litheblas.blasbasen.models import User, Section, Post, Assignment, SpecialDiet, Card
 from litheblas.mailing.models import Membership as MailingMembership
+from litheblas.watcher.models import Watcher
 
 class AssignmentInline(admin.TabularInline):
     model = Assignment
@@ -11,10 +13,29 @@ class MailingListInline(admin.TabularInline):
 class CardInline(admin.TabularInline):
     model = Card
     extra = 1
+
+class PostInline(admin.TabularInline):
+    model = Post
     
+class UserInline(admin.TabularInline):
+    model = User.groups.through
+    readonly_fields = ['user']
+    extra = 0
+
+class WatcherInline(admin.TabularInline):
+    model = Watcher
+
+class GroupAdmin(admin.ModelAdmin):
+    inlines = [WatcherInline,UserInline]
+    exclude = ('groups',)
+
+class SectionAdmin(admin.ModelAdmin):
+    inlines = [PostInline]
+
 class UserAdmin(admin.ModelAdmin):
     inlines = [AssignmentInline, CardInline, MailingListInline]
     
+    list_display = ('first_name', 'nickname', 'last_name')
 #     fieldsets = (
 #         (None, {
 #             'fields': (('first_name', 'nickname', 'last_name'), ('date_of_birth', 'personal_id_number'))
@@ -24,8 +45,10 @@ class UserAdmin(admin.ModelAdmin):
 #         })
 #     )
 
+admin.site.unregister(Group)
+admin.site.register(Group, GroupAdmin)
 admin.site.register(User, UserAdmin)
-admin.site.register(Post)
-admin.site.register(Section)
+#admin.site.register(Post)
+admin.site.register(Section, SectionAdmin)
 #admin.site.register(Assignment)
 admin.site.register(SpecialDiet)
--- watcher/admin.py
+++ watcher/admin.py
@@ -1,4 +1,23 @@
+# -*- coding: utf-8 -*-
+
 from django.contrib import admin
 from litheblas.watcher.models import Watcher
 
-admin.site.register(Watcher)
+class WatcherAdmin(admin.ModelAdmin):
+    list_display = ('description', 'group', 'list')
+    
+    filter_horizontal = ['sections', 'posts']
+    fieldsets = (
+        (None, {
+            'fields': ('description',)}),
+        ('Hämta användare från...', {
+            'description': '#TODO: En bra beskrivning här',
+            'fields': ('sections', 'posts')}),
+        ('Alternativ', {
+            'fields': ('current',)}),
+        ('Lägg till användare i...', {
+            'description': 'Välj i vilken grupp och/eller vilken mailinglista valda sektions-/postmedlemmar ska hamna.',
+            'fields': ('group', 'list')})
+    )
+
+admin.site.register(Watcher, WatcherAdmin)
