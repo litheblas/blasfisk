674cd4609cb608f4dad5addfbf731298d5a877ad
ovidner <olle@vidner.se>
2014-01-25 23:45:03 +0100

    Blog-appen inaktiverad

--- settings.py
+++ settings.py
@@ -239,7 +239,7 @@ INSTALLED_APPS = (
     "mezzanine.conf",
     "mezzanine.core",
     "mezzanine.generic",
-    "mezzanine.blog",
+    #"mezzanine.blog",
     "mezzanine.forms",
     "mezzanine.pages",
     "mezzanine.galleries",
