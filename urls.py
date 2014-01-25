413f57ffbce936f3b9b9931c8d1a8f3438e63ef7
ovidner <olle@vidner.se>
2014-01-25 01:50:25 +0100

    Tog bort en trasig import

--- urls.py
+++ urls.py
@@ -4,8 +4,6 @@ from django.contrib import admin
 
 from mezzanine.core.views import direct_to_template
 
-from litheblas.blasbasen import urls as blasbasen_urls
-
 admin.autodiscover()
 
 # Add the urlpatterns for any custom Django applications here.
