a0898a9604220eb66473f5ebc3e1d76c0a905964
ovidner <olle@vidner.se>
2014-05-16 23:22:58 +0200

    Skapat enkla felsidor samt en Bootstrap-testsida

--- litheblas/urls.py
+++ litheblas/urls.py
@@ -1,6 +1,9 @@
+# -*- coding: utf-8 -*-
+
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.generic import TemplateView
 
 from cms.sitemaps import CMSSitemap
 
@@ -13,6 +16,13 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^400/$', TemplateView.as_view(template_name='400.html')), #Test för 404-sidan
+    url(r'^403/$', TemplateView.as_view(template_name='403.html')), #Test för 500-sidan
+    url(r'^404/$', TemplateView.as_view(template_name='404.html')), #Test för 404-sidan
+    url(r'^500/$', TemplateView.as_view(template_name='500.html')), #Test för 500-sidan
+    url(r'^test/$', TemplateView.as_view(template_name='test.html')), #Test för 500-sidan
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+    url(r'^login/$', 'django.contrib.auth.views.login'),
     url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': {'cmspages': CMSSitemap}}),
     url(r'^', include('cms.urls')),
 )
