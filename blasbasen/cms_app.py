ec8f88c1c0105a21f6b5523bbe244d420a379de8
ovidner <olle@vidner.se>
2014-05-28 14:01:49 +0200

    Lagt till namespace

--- blasbasen/cms_app.py
+++ blasbasen/cms_app.py
@@ -4,7 +4,8 @@ from cms.apphook_pool import apphook_pool
 from django.utils.translation import ugettext_lazy as _
 
 class BlasbasenApp(CMSApp):
-    name = _('Blasbasen')
+    name = _(u'Blåsbasen') #Läsbart namn
     urls = ['blasbasen.urls']
+    app_name = 'blasbasen' #Viktigt! Behövs för att kunna slå upp URL:er baklänges
     
 apphook_pool.register(BlasbasenApp)
