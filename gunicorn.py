819fd795c3e24bc1a43eed68fd1c5b8f274d3941
ovidner <olle@vidner.se>
2014-05-31 21:26:40 +0200

    Tagit bort user=nobody

--- gunicorn.py
+++ gunicorn.py
@@ -1,5 +1,7 @@
+import multiprocessing
+
 command = '/opt/litheblas.org/bin/gunicorn'
 pythonpath = '/opt/litheblas.org/litheblas'
-bind = '127.0.0.1:8001'
-workers = 5
-user = 'nobody'
+bind = '127.0.0.1:8000'
+workers = multiprocessing.cpu_count() * 2 + 1
+#user = 'nobody'
