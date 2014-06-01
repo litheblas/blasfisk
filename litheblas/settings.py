151fc1c27eb2ba4e0df0c0b4f17f49eeaded3e12
ovidner <olle@vidner.se>
2014-06-01 13:52:55 +0200

    För test, ta bort innan produktion!

--- litheblas/settings.py
+++ litheblas/settings.py
@@ -31,7 +31,8 @@ DEBUG = False
 TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = ['localhost',
-                 '127.0.0.1']
+                 '127.0.0.1',
+                 'dev.skorpan.lysator.liu.se:8000']
 
 #DATABASES = {
 #    'default': {
