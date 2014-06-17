a5d2595ab77d44b9496e60494b7287494bf1da6b
ovidner <olle@vidner.se>
2014-06-17 13:10:25 +0200

    Verkställ på alla upplösningar

--- litheblas/static/js/pull-down.js
+++ litheblas/static/js/pull-down.js
@@ -10,9 +10,12 @@
 
             //set its margin-top to the difference between its own height and the height of its parent
             $('.pull-down').each(function () {
-                if ($(window).innerWidth() >= 768) {
+                
+                $(this).css('margin-top', $(this).parent().height() - $(this).height());
+                
+                /* if ($(window).innerWidth() >= 768) {
                     $(this).css('margin-top', $(this).parent().height() - $(this).height());
-                }
+                } */
             });
         }
 
