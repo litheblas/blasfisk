8592739045c4f6b6e0a6f67710b0cdca24d5f4d6
ovidner <olle@vidner.se>
2014-06-17 13:12:09 +0200

    Bind endast till 127.0.0.1

--- config/gunicorn.py
+++ config/gunicorn.py
@@ -2,7 +2,7 @@ import multiprocessing
 
 command = '/opt/litheblas.org/bin/gunicorn'
 pythonpath = '/opt/litheblas.org/litheblas'
-bind = '0.0.0.0:8000'
+bind = '127.0.0.1:8000'
 workers = multiprocessing.cpu_count() * 2 + 1
 user = None
 logfile = '/opt/litheblas.org/log/gunicorn/gunicorn.log'
