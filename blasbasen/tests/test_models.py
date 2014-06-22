093dc6c0c44ed89a4ee9a5907840f1fc99bd7985
ovidner <olle@vidner.se>
2014-06-22 17:40:46 +0200

    Paketstruktur för tester

---
+++ blasbasen/tests/test_models.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from django.test import TestCase
+
+import datetime
+from django.utils import timezone
+
+from blasbasen.models import *
+
+class PersonTestCase(TestCase):
+    def __init__(self):
+        pass
+        #setup()
