2edb88c0bb3d0607b2e100aedd533e7c227aef86
ovidner <olle@vidner.se>
2014-07-25 17:02:09 +0200

    Person relation moved from abstract model

--- cards/models.py
+++ cards/models.py
@@ -8,7 +8,6 @@ from django.utils.encoding import python_2_unicode_compatible
 class Card(models.Model):
     enabled = models.BooleanField(default=True, verbose_name=_('enabled'),
                                   help_text=_(u"Avmarkera om du tillfälligt vill spärra ditt kort"))
-    person = models.ForeignKey('blasbase.Person', related_name='cards')  # Kort _måste_ associeras med en person. Låt det vara så så slipper vi "temporära lösningar" och vilsna kort som ingen vet vem de tillhör.
     description = models.CharField(max_length=256, blank=True, verbose_name=_('description'),
                                    help_text=_(u"Anges förslagsvis om du har fler än ett kort"))
 
@@ -20,8 +19,10 @@ class Card(models.Model):
 
 
 class MagnetCard(Card):
+    person = models.ForeignKey('blasbase.Person', related_name='magnet_cards')  # Kort _måste_ associeras med en person. Låt det vara så så slipper vi "temporära lösningar" och vilsna kort som ingen vet vem de tillhör.
     card_data = models.CharField(max_length=256, verbose_name=_('card data'))
 
 
 class RFIDCard(Card):
+    person = models.ForeignKey('blasbase.Person', related_name='rfid_cards')  # Kort _måste_ associeras med en person. Låt det vara så så slipper vi "temporära lösningar" och vilsna kort som ingen vet vem de tillhör.
     card_data = models.CharField(max_length=512, verbose_name=_('card data'))
