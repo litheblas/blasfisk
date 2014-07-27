7c5623fbf3ab9ec2ed129b21002fa55c1838bcdb
Kristoffer Nilsson <talkmill@gmail.com>
2014-07-27 11:33:20 +0200

    Added aditional fields

--- blasbassync/management/commands/syncpersons.py
+++ blasbassync/management/commands/syncpersons.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 
 from django.core.management.base import BaseCommand, CommandError
-from blasbase.models import Person
+from blasbase.models import Person, SpecialDiet, PersonAddress
+from datetime import datetime
 import MySQLdb
 
 class Command(BaseCommand):
     help = 'Syncronizes persons from mysql'
 
     def handle(self, *args, **options):
+        gluten = SpecialDiet.objects.get_or_create(name='Glutenallergi')[0]
+        veg = SpecialDiet.objects.get_or_create(name='Vegetarian')[0]
+        nykter = SpecialDiet.objects.get_or_create(name='Nykterist')[0]
         print u"Öppnar mysqldatabasen"
         db = MySQLdb.connect(host="localhost", # your host, usually localhost
                      user="blasare", # your username
@@ -15,157 +19,77 @@ class Command(BaseCommand):
                       db="litheblas",
                       port=3307,
                       charset='utf8' ) # name of the data base
-        print "Hämtar data från mysql"
+        print u"Hämtar data från mysql"
         cur = db.cursor()
-        cur.execute("SELECT fnamn,smek,enamn,kon FROM person")
+        cur.execute("SELECT fnamn,smek,enamn,kon,fodd,pnr_sista,studentid,fritext,allergi,gluten,veg,nykter,gatuadr,postnr,ort,land FROM person")
         for row in cur.fetchall() :
-            test = Person()
-            test.first_name = row[0]
-            test.nickname = row[1]
-            test.last_name = row[2]
-            if row[3] == 'M':
-                test.gender = 'm'
-            elif row[3] == 'K':
-                test.gender = 'f'
+            person = Person()
+            person.first_name = row[0]
+            if row[1]:
+                person.nickname = row[1]
             else:
-                test.gender = None
+                person.nickname = ""
+            person.last_name = row[2]
+            if row[3] == 'M':
+                person.gender = 'm'
+            elif row[3] == 'K':
+                person.gender = 'f'
+            else:
+                person.gender = ""
+            person.born = row[4]
+            person.deceased = None
+            if row[5]:
+                person.personal_id_number = row[5]
+            else:
+                person.personal_id_number = ""
+            if row[6]:
+                person.liu_id = row[6]
+            else:
+                person.liu_id = ""
+            if row[7]:
+                person.about = row[7]
+            else:
+                person.about = ""
+            if row[8]:
+                person.special_diets_extra = row[8]
+            else:
+                person.special_diets_extra = ""
+            person.save()
+            if row[9] == 'Y':
+                person.special_diets.add(gluten)
+            if row[10] == 'Y':
+                person.special_diets.add(veg)
+            if row[11] == 'Y':
+                person.special_diets.add(nykter)
+            paddress = PersonAddress()
+            paddress.type = 'private'
+            paddress.person = person
+            if row[12]:
+                paddress.address = row[12]
+            else:
+                paddress.address = ""
+            if row[13]:
+                paddress.post_code = row[13]
+            else:
+                paddress.post_code = ""
+            if row[14]:
+                paddress.city = row[14]
+            else:
+                paddress.city = ""
+            if row[15]:
+                paddress.country = row[15]
+            else:
+                paddress.country = ""
+            paddress.save()
 
-            print test
-            print test.gender
-
-
-
-
-
-"""class Person(models.Model):
-    Lagrar personer (som i människor) och är den datatyp som nästan allt i Blåsbasen kopplas till.
-
- X   first_name = models.CharField(max_length=256, verbose_name=_('first name'))
- X   nickname = models.CharField(max_length=256, blank=True, verbose_name=_('nickname'))
- X   last_name = models.CharField(max_length=256, verbose_name=_('last name'),
-                                 help_text=u'Ange gärna tidigare efternamn inom parentes, t.ex. Vidner (Eriksson)')
- X   gender = models.CharField(max_length=1, choices=GENDERS, blank=True, verbose_name=_('gender'))
-    born = models.DateField(blank=True, null=True, verbose_name=_('born'),
-                            validators=[validators.date_before_today])
-    deceased = models.DateField(blank=True, null=True, verbose_name=_('deceased'),
-                                validators=[validators.date_before_today])
-    personal_id_number = models.CharField(max_length=4, blank=True, verbose_name=_('personal identification number'),
-                                          help_text=u'Sista 4 siffrorna i personnumret')  # Last 4 characters in Swedish personal id number
-    liu_id = models.CharField(max_length=8, blank=True, verbose_name=_('LiU-ID'))
-
-
-    about = models.TextField(blank=True, verbose_name=_('about'))
-
-
-    special_diets = models.ManyToManyField('SpecialDiet', related_name='people', blank=True, null=True, verbose_name=_('special diets'))
-    special_diets_extra = models.CharField(max_length=256, blank=True, verbose_name=_('special diets comments'))
-
-
-    posts = models.ManyToManyField('Post', through='Assignment', verbose_name=_('posts'))
-
-
-    last_updated = models.DateTimeField(auto_now=True, verbose_name=_('last updated'))
-
-
-    # Låt oss fortsätta kalla den objects istället för typ people, så hålls det konsekvent mellan alla modeller
-    objects = PersonManager()
-
-
-    class Meta:
-        ordering = ['first_name', 'last_name', 'nickname']
-        verbose_name = _('person')
-        verbose_name_plural = _('people')
-
-
-    def __str__(self):
-        return self.get_full_name()
-
-
-    def clean(self):
-        cleaned_data = super(Person, self).clean()
-
-
-        # Validera endast om både födelse- och dödsdatum angetts.
-        if self.born and self.deceased:
-            validators.datetime_before_datetime(self.born, self.deceased, _(u'Decease date must be after birth date.'))
-        return cleaned_data
+            """Not yet added """
+            """
+               test.posts
+               """
+            person.save()
 
 
-    def get_age(self):
-        if self.born and not self.deceased:
-            return relativedelta(datetime.date.today(), self.born)
-        elif self.born and self.deceased:
-            return relativedelta(self.deceased, self.born)
-        return None
 
 
-    @property
-    def memberships(self):
-        return self.assignments.memberships()
-
-
-    @property
-    def engagements(self):
-        return self.assignments.engagements()
-
-
-    def get_primary_avatar(self):
-        return self.avatars.get(primary=True)
-
-
-    def get_secondary_avatars(self):
-        return self.avatars.exclude(primary=True)
-
-
-    # Används internt av Django
-    def get_full_name(self):
-        if self.nickname:
-            return u'{0} "{1}" {2}'.format(self.first_name, self.nickname, self.last_name)  # Leif "Pappa Blås" Holm
-
-
-        return u'{0} {1}'.format(self.first_name, self.last_name)  # Leif Holm
-
-
-    # Används internt av Django
-    def get_short_name(self):
-        if self.nickname:
-            return self.nickname  # Pappa Blås
-
-
-        return u'{0} {1}'.format(self.first_name, self.last_name[0])  # Leif H
-
-
-    @property
-    def start_date(self):
-        Hämtar alla assignments som innebär medlemsskap och som inte är provmedlemsskap,
-        sorterar stigande på startdatum, tar det första objektet och ger detta objekts startdatum
-        try:
-            return self.assignments.memberships().first().start_date
-        except AttributeError:
-            # Om man inte har något assignment som passar i filtret alls måste vi hantera det felet på något vis. Då
-            # returnerar vi None istället.
-            return None
-
-
-    @property
-    def end_date(self):
-        a = self.assignments.memberships().order_by('end_date')
-
-
-        # Om man inte har några sådana assignments eller om någon är pågående returnerar vi None
-        if not a or a.filter(pk__in=Assignment.objects.active()):
-            return None
-
-
-        # Annars tar vi det sista objektet och ger dess slutdatum
-        return a.last().end_date
-
-
-    age = property(get_age)
-    full_name = property(get_full_name)
-    short_name = property(get_short_name)
-    primary_avatar = property(get_primary_avatar)
-    secondary_avatars = property(get_secondary_avatars)
-"""
 
 
---
+++ newsync.bat
@@ -0,0 +1,3 @@
+del db.sqlite3
+copy db.sqlite3.bak2 db.sqlite3
+python manage.dev.py syncpersons
