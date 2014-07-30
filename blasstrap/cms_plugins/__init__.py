39624cf0e7b227b75ca8e75b16a90823859ec7df
Olle Vidner <olle@vidner.se>
2014-07-30 13:44:17 +0200

    Removed invalid import

--- blasstrap/cms_plugins/__init__.py
+++ blasstrap/cms_plugins/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from blasstrap.cms_plugins.carousel import *
-from blasstrap.cms_plugins.generic import *
 from blasstrap.cms_plugins.jumbotron import *
 from blasstrap.cms_plugins.parallax import *
 from blasstrap.cms_plugins.pusher import *
