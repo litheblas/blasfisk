73aab34139f16ef07643647146e2034531e1c518
Kristoffer Nilsson <talkmill@gmail.com>
2014-08-02 20:54:04 +0200

    Changed post-variable to function in assignment

--- blasbase/tests/test_models.py
+++ blasbase/tests/test_models.py
@@ -11,17 +11,25 @@ from blasbase.models import *
 class PersonMethodsTestCase(TestCase):
     @classmethod
     def setUpClass(cls):
+        testFunktion = Function()
+        testFunktion.name = "Trädgårdsmästare"
+        testFunktion.engagement = True
+        testFunktion.save()
+        testFunktion2 = Function()
+        testFunktion2.name = "Triangel"
+        testFunktion2.membership = True
+        testFunktion2.save()
+        testFunktion3 = Function()
+        testFunktion3.name = "bollkalle"
+        testFunktion3.save()
         person = Person(first_name=u'Niklas', last_name=u'Namnlös')
         #måste lägga in en avatar
         person.save()
+
+
     def setUp(self):
         self.person = Person.objects.get(pk=1)
-        """
-        Post is removed, need to work with assignment/function
-        self.post_nothing = Post.objects.get(name='nothing')
-        self.post_membership = Post.objects.get(name='membership')
-        self.post_engagement = Post.objects.get(name='engagement')
-        self.post_both = Post.objects.get(name='both')"""
+
 
     def test_age_without_born_or_deceased(self):
         self.assertIsNone(self.person.age)
@@ -79,61 +87,61 @@ class PersonMethodsTestCase(TestCase):
         self.person.nickname = 'Nick'
         self.person.save()
         self.assertEqual(self.person.short_name, 'Nick')
-"""
-        Post is removed, need to rewrite
+
     def test_start_date_with_non_member(self):
 
-        Assignment.objects.create(person=self.person, post=self.post_nothing,
-                                  start_date=(datetime.date.today() - relativedelta(months=1)))
+        Assignment.objects.create(person=self.person, function=Function.objects.get(name='bollkalle'),start=(datetime.date.today() - relativedelta(months=1)))
         self.assertIsNone(self.person.start_date)
 
+
     def test_start_date_with_trial_member(self):
-        Assignment.objects.create(person=self.person, post=self.post_membership, trial=True,
-                                  start_date=(datetime.date.today() - relativedelta(months=1)))
+        Assignment.objects.create(person=self.person, function=Function.objects.get(name='Triangel'), trial=True,
+                                  start=(datetime.date.today() - relativedelta(months=1)))
         self.assertIsNone(self.person.start_date)
 
     def test_start_date_with_active_member(self):
-        Assignment.objects.create(person=self.person, post=self.post_membership,
-                                  start_date=(datetime.date.today() - relativedelta(months=1)))
+        Assignment.objects.create(person=self.person, function=Function.objects.get(name='Triangel'),
+                                  start=(datetime.date.today() - relativedelta(months=1)))
         self.assertEqual(self.person.start_date, (datetime.date.today() - relativedelta(months=1)))
 
+
     def test_start_date_with_oldie(self):
-        Assignment.objects.create(person=self.person, post=self.post_membership,
-                                  start_date=(datetime.date.today() - relativedelta(years=10)),
-                                  end_date=(datetime.date.today() - relativedelta(years=1)))
+        Assignment.objects.create(person=self.person, function=Function.objects.get(name='Triangel'),
+                                  start=(datetime.date.today() - relativedelta(years=10)),
+                                  end=(datetime.date.today() - relativedelta(years=1)))
         self.assertEqual(self.person.start_date, (datetime.date.today() - relativedelta(years=10)))
 
     def test_start_date_with_returning_member(self):
-        Assignment.objects.create(person=self.person, post=self.post_membership,
-                                  start_date=(datetime.date.today() - relativedelta(years=10)),
-                                  end_date=(datetime.date.today() - relativedelta(years=3)))
-        Assignment.objects.create(person=self.person, post=self.post_membership,
-                                  start_date=(datetime.date.today() - relativedelta(years=2)))
+        Assignment.objects.create(person=self.person, function=Function.objects.get(name='Triangel'),
+                                  start=(datetime.date.today() - relativedelta(years=10)),
+                                  end=(datetime.date.today() - relativedelta(years=3)))
+        Assignment.objects.create(person=self.person, function=Function.objects.get(name='Triangel'),
+                                  start=(datetime.date.today() - relativedelta(years=2)))
         self.assertEqual(self.person.start_date, (datetime.date.today() - relativedelta(years=10)))
 
     def test_end_date_with_non_member(self):
-        Assignment.objects.create(person=self.person, post=self.post_nothing,
-                                  start_date=(datetime.date.today() - relativedelta(years=1)),
-                                  end_date=(datetime.date.today() - relativedelta(months=1)))
+        Assignment.objects.create(person=self.person, function=Function.objects.get(name='bollkalle'),
+                                  start=(datetime.date.today() - relativedelta(years=1)),
+                                  end=(datetime.date.today() - relativedelta(months=1)))
         self.assertIsNone(self.person.end_date)
 
     def test_start_date_with_trial_member(self):
-        Assignment.objects.create(person=self.person, post=self.post_membership, trial=True,
-                                  start_date=(datetime.date.today() - relativedelta(years=1)),
-                                  end_date=(datetime.date.today() - relativedelta(months=1)))
+        Assignment.objects.create(person=self.person, function=Function.objects.get(name='Triangel'), trial=True,
+                                  start=(datetime.date.today() - relativedelta(years=1)),
+                                  end=(datetime.date.today() - relativedelta(months=1)))
         self.assertIsNone(self.person.start_date)
 
     def test_end_date_with_active_member(self):
-        Assignment.objects.create(person=self.person, post=self.post_membership,
-                                  start_date=(datetime.date.today() - relativedelta(months=1)))
+        Assignment.objects.create(person=self.person, function=Function.objects.get(name='Triangel'),
+                                  start=(datetime.date.today() - relativedelta(months=1)))
         self.assertIsNone(self.person.end_date)
 
     def test_end_date_with_oldie(self):
-        Assignment.objects.create(person=self.person, post=self.post_membership,
-                                  start_date=(datetime.date.today() - relativedelta(years=10)),
-                                  end_date=(datetime.date.today() - relativedelta(years=1)))
+        Assignment.objects.create(person=self.person, function=Function.objects.get(name='Triangel'),
+                                  start=(datetime.date.today() - relativedelta(years=10)),
+                                  end=(datetime.date.today() - relativedelta(years=1)))
         self.assertEqual(self.person.end_date, (datetime.date.today() - relativedelta(years=1)))
 
     def test_assignments_returns_only_active(self):
 
-        self.assertQuerysetEqual(self.person.assignments, )"""
+        self.assertQuerysetEqual(self.person.assignments, )
