7f75311d5759da5b6e6001a0875555a48efecf61
Kristoffer Nilsson <talkmill@gmail.com>
2014-08-03 20:12:31 +0200

    Added a testrunner for manage.test.py where the applications to test are explicitely listed

--- litheblas/settings/test.py
+++ litheblas/settings/test.py
@@ -32,8 +32,15 @@ CMS_CACHE_DURATIONS = {
 }
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'litheblas', 'media')
-
-
+TEST_RUNNER = 'litheblas.includeappstestsuiterunner.IncludeAppsTestSuiteRunner'
+APPS_TO_TEST = (
+    'blasstrap',
+    'blasbase',
+    'cards',
+    'events',
+    'locations',
+    'contact',
+)
 # MUST BE LAST IN FILE
 try:
     from litheblas.settings.local import *
