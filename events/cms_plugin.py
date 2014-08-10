cc8b8f92334b1619bd2827147537bd37809f5d5e
Olle Vidner <olle@vidner.se>
2014-08-10 14:29:46 +0200

    Boilerplate for event viewer plugin

---
+++ events/cms_plugin.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from django.utils.translation import ugettext_lazy as _
+
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+
+from events.models import Event, EventViewer
+
+
+class PublicEventViewerPlugin(CMSPluginBase):
+    name = _('Public events')
+    module = _('Events')
+
+    model = EventViewer
+
+    render_template = 'plugins/event_viewer.html'
+    allow_children = False
+
+    def render(self, context, instance, placeholder):
+        context = super(PublicEventViewerPlugin, self).render(context, instance, placeholder)
+
+        context['events'] = Event.objects.filter(event_type__in=instance.event_types.all()).public().future().limit_to()[:instance.count]
+        return context
+
+plugin_pool.register_plugin(PublicEventViewerPlugin)
