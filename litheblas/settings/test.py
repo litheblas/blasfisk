45ef520914960fb32e7407e02261d8ada16414af
Olle Vidner <olle@vidner.se>
2014-08-10 13:23:49 +0200

    Removed redundant settings from test settings

--- litheblas/settings/test.py
+++ litheblas/settings/test.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from litheblas.settings import *
+from litheblas.settings.dev import *
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = True
 
 CRISPY_FAIL_SILENTLY = False
@@ -15,27 +15,3 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-SECRET_KEY = 'd*dvb_n#28xx9ij*f(-5=*9k9s57_=#!rqgrjl!2$&+b=%ujr$'
-DATABASE_PASSWORD = None
-
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
-    }
-}
-
-CMS_CACHE_DURATIONS = {
-    'content': 0,
-    'menus': 0,
-    'permissions': 0
-}
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'litheblas', 'media')
-
-# MUST BE LAST IN FILE
-try:
-    from litheblas.settings.local import *
-except ImportError:
-    pass
-# MUST BE LAST IN FILE__author__ = 'Kristoffer'
