75755b9e2315b6193d6631c8430af04e650a1b6d
Olle Vidner <olle@vidner.se>
2014-08-10 21:29:31 +0200

    Changed log file name

--- conf/gunicorn.py
+++ conf/gunicorn.py
@@ -6,6 +6,6 @@ pythonpath = '/opt/blasfisk/blasfisk'
 bind = '127.0.0.1:8000'
 workers = multiprocessing.cpu_count() * 2 + 1
 user = None
-logfile = '/opt/venvs/blasfisk/log/gunicorn/gunicorn.log'
+logfile = '/opt/venvs/blasfisk/log/gunicorn/blasfisk.log'
 loglevel = 'debug'
 worker_tmp_dir = '/opt/venvs/blasfisk/tmp/'
