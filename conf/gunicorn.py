95b89121056f7e3d8066ad4225067f3f543337af
Olle Vidner <olle@vidner.se>
2014-08-10 21:47:25 +0200

    Reverted b33d59fa337c7fdffe83723d92a53f574dda545e

--- conf/gunicorn.py
+++ conf/gunicorn.py
@@ -3,7 +3,7 @@ import multiprocessing
 
 command = '/opt/venvs/blasfisk/bin/gunicorn'
 pythonpath = '/opt/blasfisk/blasfisk'
-bind = '0.0.0.0:8000'
+bind = '127.0.0.1:8000'
 workers = multiprocessing.cpu_count() * 2 + 1
 user = None
 logfile = '/opt/venvs/blasfisk/log/gunicorn/blasfisk.log'
