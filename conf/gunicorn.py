2b967d3ec6fee1924c48589d1d91964ec0104055
Olle Vidner <olle@vidner.se>
2014-08-10 18:45:09 +0200

    Updated supervisor and gunicorn conf

--- conf/gunicorn.py
+++ conf/gunicorn.py
@@ -1,10 +1,11 @@
+# -*- coding: utf-8 -*-
 import multiprocessing
 
-command = '/opt/litheblas.org/bin/gunicorn'
-pythonpath = '/opt/litheblas.org/litheblas'
+command = '/opt/venvs/blasfisk/bin/gunicorn'
+pythonpath = '/opt/blasfisk/blasfisk'
 bind = '127.0.0.1:8000'
 workers = multiprocessing.cpu_count() * 2 + 1
 user = None
-logfile = '/opt/litheblas.org/log/gunicorn/gunicorn.log'
+logfile = '/opt/venvs/blasfisk/log/gunicorn/gunicorn.log'
 loglevel = 'debug'
-worker_tmp_dir = '/opt/litheblas.org/tmp/'
+worker_tmp_dir = '/opt/venvs/blasfisk/tmp/'
