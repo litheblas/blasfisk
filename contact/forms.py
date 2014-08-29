e58f46d793e3ef4e2edc4edae0ebde8b7538ead0
Olle Vidner <olle@vidner.se>
2014-08-29 13:20:49 +0200

    Changed to lazy translation and updated

--- contact/forms.py
+++ contact/forms.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django import forms
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit, Layout, Div, Field
 from django.http import HttpResponseRedirect
